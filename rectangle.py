2a139968f2b4c65db1c19e1b93d024508b141751
DerekG06 <dguzmanfernandez@students.mchenry.edu>
2025-02-03 20:37:00 -0600

    Rectangle 2.3

---
+++ rectangle.py
@@ -0,0 +1,14 @@
+
+
+# Length variable with an integer value
+length = int(5)
+
+# Width variable with another integer value
+width = int(10)
+
+# Both variables multiplied by each other, product is stored in "area"
+area = length * width
+
+# Print statement that auto converts the integers to strings.
+print(f"The area of a rectangle with the length of {
+      length} and a width of {width} is {area}.")
