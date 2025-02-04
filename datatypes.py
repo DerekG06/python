ed6b78d276121bff16d3464a1ff607fa58b751fd
DerekG06 <dguzmanfernandez@students.mchenry.edu>
2025-02-03 20:23:07 -0600

    PE1 2.2 Data Types

---
+++ datatypes.py
@@ -0,0 +1,22 @@
+
+
+# This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 3)
+
+# Integer
+y = 57
+print("Integer:", y)
+
+# Float
+z = 37.50
+print("Float:", z)
+
+# String
+name = "Derek"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
